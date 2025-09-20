d7dc03a0b178b9ba956cc2d12df72ffc1c2d860a
Danil Romanov <danillyou@mail.ru>
2025-09-20 14:14:04 +0300

    First HW

--- inf403_lab01/src/main/java/net_HW/HttpServer.java
+++ inf403_lab01/src/main/java/net_HW/HttpServer.java
@@ -9,53 +9,29 @@ import org.apache.logging.log4j.Logger;
 
 
 public class HttpServer {
-    private static final Logger log = LogManager.getLogger(HttpServer.class);
+
+    final static Logger logger = LogManager.getLogger(HttpServer.class);
 
     public static void main(String[] args) {
-        log.info("Starting HttpServer");
+
+        new Application().init();
+
+        //Создается единый обработчик запросов
+        RequestHandler requestHandler = new RequestHandler();
+        logger.info("start HttpServer");
         try {
-
-
             ServerSocket serverSocket = new ServerSocket(8080);
-            // Ожидаем подключения клиента
             while (true) {
+                // Ожидаем подключения клиента
                 Socket clientSocket = serverSocket.accept();
+
                 // Дождались клиента
-                // Поток для чтения данных от клиента
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(clientSocket.getInputStream()));
-                String lineOne = reader.readLine();
-                System.out.println(lineOne);
-                String[] components = lineOne.split(" ");
-                String resource = components[1];
-
-                if (resource.equals("/shutdown")) {
-                    break;
-                }
-
-                while (true) {
-                    // Читаем пакет от клиента
-                    String message = reader.readLine();
-                    System.out.println(message);
-
-                    if (message.isEmpty()) {
-                        OutputStream os = clientSocket.getOutputStream();
-                        os.write("HTTP/1.1 200 OK\r\n".getBytes());
-                        os.write("Content-Type: text/html;charset=UTF-8\r\n".getBytes());
-                        os.write("\r\n".getBytes());
-                        os.write("<html><body>Hello!</body></html>".getBytes());
-                        os.flush();
-                        break;
-                    }
-                }
-                clientSocket.close();
+                new Thread(() -> requestHandler.handle(clientSocket)).start();
             }
-            serverSocket.close();
-
-
+            //serverSocket.close();
         } catch (IOException e) {
+            logger.atError().withThrowable(e);
             throw new RuntimeException(e);
         }
-
     }
 }
